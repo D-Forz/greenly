e51e7711041046c24e0e4e3f0086835384d2b98a
duvan <duvanfoz@hotmail.com>
2022-10-14 11:02:11 -0500

    update: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '/', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+
   root to: "pages#home"
 
   resources :events do
@@ -7,4 +8,5 @@ Rails.application.routes.draw do
   end
 
   resources :posts, only: %i[index]
+  resources :comments, only: %i[create update destroy]
 end

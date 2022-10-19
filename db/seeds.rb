87595cb258c49ef67c595b2e130ef45ec3ea5b2d
Felipe Tavera <jftaa@hotmail.com>
2022-10-18 20:30:11 -0500

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,14 @@
 puts "\n== Seeding the database with fixtures =="
 system("rails db:fixtures:load")
+
+p 'creando'
+
+User.create!(
+  first_name: "admin",
+  last_name: "greenly",
+  username: "admincito",
+  organizer: true,
+  email: "jhon@email.com",
+  encrypted_password:  Devise::Encryptor.digest(User, 'password') 
+)
+p 'creando usuario'

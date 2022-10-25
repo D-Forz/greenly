90fd2bd0f9a280d73ffdfb12d8d3dd6633346dbc
duvan <duvanfoz@hotmail.com>
2022-10-25 11:51:11 -0500

    feat: set meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,10 @@ class ApplicationController < ActionController::Base
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   protected
 
   def configure_permitted_parameters

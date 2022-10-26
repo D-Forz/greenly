bfccc41e43fc562d4511582cf7081e0af109b9a4
duvan <duvanfoz@hotmail.com>
2022-10-26 15:48:21 -0500

    fix: pagy countless

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = policy_scope(Post).includes(:user, :photo_attachment, :comments).order(created_at: :desc)
-    @pagy, @posts = pagy(@posts, items: 10)
+    @pagy, @posts = pagy_countless(@posts, items: 10)
     @events = policy_scope(Event).order(event_date: :asc).first(3)
   end
 

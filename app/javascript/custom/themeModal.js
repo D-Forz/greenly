373c089bb7dd98bb3aedef16d1705a3a44b9420d
Daniel Amaya <daniel.amaya.buitrago@outlook.com>
2022-10-29 12:02:07 -0500

    [ADD] theme modal open&close no logic yet

---
+++ app/javascript/custom/themeModal.js
@@ -0,0 +1,18 @@
+const theme = document.querySelector("#theme");
+const themeModal = document.querySelector(".customize-theme");
+
+//theme customization starts
+//opens modal
+const openThemeModal = () => {
+  themeModal.style.display = "grid";
+};
+// closes modal
+const closeThemeModal = (e) => {
+  if (e.target.classList.contains("customize-theme")) {
+      themeModal.style.display = "none";
+  }
+};
+themeModal.addEventListener("click", closeThemeModal);
+
+theme.addEventListener("click", openThemeModal);
+//theme customization ends
